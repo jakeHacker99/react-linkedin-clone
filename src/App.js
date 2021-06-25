5036f2b22986d3d8741ec530dce26fd44fdf7f8f
jakeHacker99 <jakob.yaro@utb.ecutbildning.se>
2021-06-26 01:35:51 +0200

    impl login

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
+import Login from "./Components/Login";
 
 function App() {
-  return <div className="App">lets build the linedin clone 🔥🔥🔥🔥 </div>;
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Login />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;

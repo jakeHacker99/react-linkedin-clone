6e02cbcf3fc562c944402d65c8203f7960b6f492
jakeHacker99 <jakob.yaro@utb.ecutbildning.se>
2021-06-27 03:20:00 +0200

    impl rightside

---
+++ src/Components/Rightside.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+const Rightside = () => {
+  return (
+    <Container>
+      <h2>rightside</h2>
+    </Container>
+  );
+};
+
+const Container = styled.div``;
+export default Rightside;

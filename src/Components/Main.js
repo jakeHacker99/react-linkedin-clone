599a30468ba455e9892aeb43cdf0e246838f5954
jakeHacker99 <jakob.yaro@utb.ecutbildning.se>
2021-06-27 03:19:38 +0200

    impl main

---
+++ src/Components/Main.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+const Main = () => {
+  return (
+    <Container>
+      <h2>main</h2>
+    </Container>
+  );
+};
+
+const Container = styled.div``;
+export default Main;
